913be3eeaa3c40a07cc8c0566438cbd2bf0fcc12
Vitor-Ugo-Vieira <145944093+Vitor-Ugo-Vieira@users.noreply.github.com>
2023-10-10 21:43:03 -0400

    Add files via upload

--- Desenvolvimento web AV1/Modelo Físico.sql
+++ Desenvolvimento web AV1/Modelo Físico.sql
@@ -1,36 +1,62 @@
-CREATE DATABASE IF NOT EXISTS FSEBVU
-DEFAULT CHARACTER SET utf8mb4
+CREATE DATABASE IF NOT EXISTS torugokv_pdv_FSEBVU 
+
+DEFAULT CHARACTER SET utf8mb4 
+
 COLLATE utf8mb4_general_ci;
 
-USE FSEBVU;
+USE torugokv_pdv_FSEBVU;
 
 CREATE TABLE usuario(
-codigo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-nome_usuario VARCHAR(50) NOT NULL,
-email VARCHAR(60) NOT NULL,
-senha VARCHAR(32) NOT NULL
+    codigo_usuario INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome_usuario VARCHAR(100) NOT NULL,
+    email VARCHAR(60) NOT NULL,
+    senha_login VARCHAR(32) NOT NULL
 );
 
-CREATE TABLE tipo_usuario(
-cadastrado VARCHAR(40) NOT NULL PRIMARY KEY,
-nao_cadastrado VARCHAR(40) NOT NULL
+CREATE TABLE receita(
+    id_receita INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(50) NOT NULL,
+    descricao VARCHAR(60) NOT NULL,
+    ingredientes VARCHAR(32) NOT NULL,
+    preparo VARCHAR(200) NOT NULL,
+    categoria VARCHAR(1) NOT NULL,
+    img_receita VARCHAR(1) NOT NULL
 );
 
-CREATE TABLE post(
-data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-hora TIMESTAMP NOT NULL,
-codigo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-titulo VARCHAR(50) NOT NULL,
-descricao VARCHAR(300) NOT NULL,
-ingredientes VARCHAR(600) NOT NULL,
-modo_de_preparo VARCHAR(1000) NOT NULL,
-FOREIGN KEY (codigo)
-REFERENCES usuario(codigo)
+CREATE TABLE avaliacao(
+    id_avaliacao INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    autor_avaliacao VARCHAR(50) NOT NULL,
+    data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    nota VARCHAR(1) NOT NULL
 );
 
-CREATE TABLE site_receita(
-nome_usuario VARCHAR(50) NOT NULL,
-codigo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-FOREIGN KEY (codigo)
-REFERENCES usuario(codigo)
+CREATE TABLE favorito(
+    id_favorito INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
+
+CREATE TABLE categoria(
+    id_categoria INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    sobremesas VARCHAR(1) NOT NULL,
+    pratos_principais VARCHAR(1) NOT NULL,
+    bebidas VARCHAR(1) NOT NULL
+);
+
+CREATE TABLE comentario(
+    id_comentario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),         /* data_comentario */
+    autor_comentario VARCHAR(1) NOT NULL
+);
+
+CREATE TABLE login(
+    codigo_login INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome_completo_login VARCHAR(100) NOT NULL,
+    email_login VARCHAR(50) NOT NULL UNIQUE,
+    senha_login VARCHAR(32) NOT NULL,
+    tipo_login INT(11) NOT NULL DEFAULT 0
+);
+
+/*lembrar do ALTER TABLE depois*/
+
+INSERT INTO login (codigo_login, nome_completo_login, email_login, senha_login, tipo_login) VALUES
+(1, 'Administrador', 'admin@gmail.com', '202cb962ac59075b964b07152d234b70', 0),
+(2, 'Usuario', 'teste@gmail.com', '202cb962ac59075b964b07152d234b70', 1);
